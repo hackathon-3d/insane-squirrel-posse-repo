1a9f87c3e8abf18d1553870c69f37fbc8d866176
LelandLong <lelandlong@me.com>
2013-08-23 23:04:53 -0400

    ViewController filename refactor

---
+++ Kids: The Users Manual/KUMDaddyViewController.m
@@ -0,0 +1,25 @@
+//
+//  KUMDaddyViewController.m
+//  Kids: The Users Manual
+//
+//  Created by admin on 8/23/13.
+//  Copyright (c) 2013 worldquest.us. All rights reserved.
+//
+
+#import "KUMDaddyViewController.h"
+
+@implementation KUMDaddyViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Kids: The Users Manual/KUMMommyViewController.m
@@ -0,0 +1,28 @@
+//
+//  KUMMommyViewController.m
+//  Kids: The Users Manual
+//
+//  Created by admin on 8/23/13.
+//  Copyright (c) 2013 worldquest.us. All rights reserved.
+//
+
+#import "KUMMommyViewController.h"
+
+@implementation KUMMommyViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    
+    //new comment
+    // hi
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
