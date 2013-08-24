e43fb81450f89a1bf14b901927c846daf41da703
LelandLong <lelandlong@me.com>
2013-08-24 02:03:57 -0400

    Added ChildList Controller

---
+++ Kids: The Users Manual/KUMChildListViewController.h
@@ -0,0 +1,13 @@
+//
+//  KUMChildListViewController.h
+//  Kids: The Users Manual
+//
+//  Created by admin on 8/24/13.
+//  Copyright (c) 2013 worldquest.us. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KUMChildListViewController : UITableViewController
+
+@end
