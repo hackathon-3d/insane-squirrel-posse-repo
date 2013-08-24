2ac898e2231e5f8a49df94adafe9efce9168de00
LelandLong <lelandlong@me.com>
2013-08-24 05:15:01 -0400

    Details connected

--- Kids: The Users Manual/KUMChildDetailViewController.h
+++ Kids: The Users Manual/KUMChildDetailViewController.h
@@ -19,4 +19,30 @@
 @property (strong, nonatomic) IBOutlet UITextField *lengthField;
 @property (strong, nonatomic) IBOutlet UITextField *weightField;
 
+@property (strong, nonatomic) IBOutlet UITextField *hepbField;
+@property (strong, nonatomic) IBOutlet UITextField *rvField;
+@property (strong, nonatomic) IBOutlet UITextField *dtapField;
+@property (strong, nonatomic) IBOutlet UITextField *hibField;
+@property (strong, nonatomic) IBOutlet UITextField *pcvField;
+@property (strong, nonatomic) IBOutlet UITextField *ipvField;
+@property (strong, nonatomic) IBOutlet UITextField *influenzaField;
+@property (strong, nonatomic) IBOutlet UITextField *mmrField;
+@property (strong, nonatomic) IBOutlet UITextField *varicellaField;
+@property (strong, nonatomic) IBOutlet UITextField *hepaField;
+
+@property (strong, nonatomic) IBOutlet UITextField *teethingField;
+@property (strong, nonatomic) IBOutlet UITextField *crawlingField;
+@property (strong, nonatomic) IBOutlet UITextField *stepsField;
+@property (strong, nonatomic) IBOutlet UITextField *daddaField;
+@property (strong, nonatomic) IBOutlet UITextField *mommaField;
+@property (strong, nonatomic) IBOutlet UITextField *timeoutField;
+@property (strong, nonatomic) IBOutlet UITextField *babysitterField;
+@property (strong, nonatomic) IBOutlet UITextField *pottyField;
+@property (strong, nonatomic) IBOutlet UITextField *toothField;
+@property (strong, nonatomic) IBOutlet UITextField *tricycleField;
+@property (strong, nonatomic) IBOutlet UITextField *bicycleField;
+@property (strong, nonatomic) IBOutlet UITextField *trainingWheelsField;
+@property (strong, nonatomic) IBOutlet UITextField *schoolField;
+@property (strong, nonatomic) IBOutlet UITextField *sleepOverField;
+
 @end
--- Kids: The Users Manual/KUMChildDetailViewController.m
+++ Kids: The Users Manual/KUMChildDetailViewController.m
@@ -29,6 +29,33 @@
     _nicknameField.text = [[_childDetailDictionary objectForKey:@"Nickname"] objectAtIndex:[appDelegate.segueParameter integerValue]];
     _lengthField.text = [[_childDetailDictionary objectForKey:@"Length"] objectAtIndex:[appDelegate.segueParameter integerValue]];
     _weightField.text = [[_childDetailDictionary objectForKey:@"Weight"] objectAtIndex:[appDelegate.segueParameter integerValue]];
+    
+    
+    _hepbField.text = [[_childDetailDictionary objectForKey:@"Shot_hepb"] objectAtIndex:[appDelegate.segueParameter integerValue]];
+    _rvField.text = [[_childDetailDictionary objectForKey:@"Shot_rv"] objectAtIndex:[appDelegate.segueParameter integerValue]];
+    _dtapField.text = [[_childDetailDictionary objectForKey:@"Shot_dtap"] objectAtIndex:[appDelegate.segueParameter integerValue]];
+    _hibField.text = [[_childDetailDictionary objectForKey:@"Shot_hib"] objectAtIndex:[appDelegate.segueParameter integerValue]];
+    _pcvField.text = [[_childDetailDictionary objectForKey:@"Shot_pcv"] objectAtIndex:[appDelegate.segueParameter integerValue]];
+    _ipvField.text = [[_childDetailDictionary objectForKey:@"Shot_ipv"] objectAtIndex:[appDelegate.segueParameter integerValue]];
+    _influenzaField.text = [[_childDetailDictionary objectForKey:@"Shot_influenza"] objectAtIndex:[appDelegate.segueParameter integerValue]];
+    _mmrField.text = [[_childDetailDictionary objectForKey:@"Shot_mmr"] objectAtIndex:[appDelegate.segueParameter integerValue]];
+    _varicellaField.text = [[_childDetailDictionary objectForKey:@"Shot_varicella"] objectAtIndex:[appDelegate.segueParameter integerValue]];
+    _hepaField.text = [[_childDetailDictionary objectForKey:@"Shot_hepa"] objectAtIndex:[appDelegate.segueParameter integerValue]];
+    
+    _teethingField.text = [[_childDetailDictionary objectForKey:@"Teething"] objectAtIndex:[appDelegate.segueParameter integerValue]];
+    _crawlingField.text = [[_childDetailDictionary objectForKey:@"Crawling"] objectAtIndex:[appDelegate.segueParameter integerValue]];
+    _stepsField.text = [[_childDetailDictionary objectForKey:@"Steps"] objectAtIndex:[appDelegate.segueParameter integerValue]];
+    _daddaField.text = [[_childDetailDictionary objectForKey:@"Dadda"] objectAtIndex:[appDelegate.segueParameter integerValue]];
+    _mommaField.text = [[_childDetailDictionary objectForKey:@"Momma"] objectAtIndex:[appDelegate.segueParameter integerValue]];
+    _timeoutField.text = [[_childDetailDictionary objectForKey:@"Timeout"] objectAtIndex:[appDelegate.segueParameter integerValue]];
+    _babysitterField.text = [[_childDetailDictionary objectForKey:@"Babysitter"] objectAtIndex:[appDelegate.segueParameter integerValue]];
+    _pottyField.text = [[_childDetailDictionary objectForKey:@"Potty"] objectAtIndex:[appDelegate.segueParameter integerValue]];
+    _toothField.text = [[_childDetailDictionary objectForKey:@"Tooth"] objectAtIndex:[appDelegate.segueParameter integerValue]];
+    _tricycleField.text = [[_childDetailDictionary objectForKey:@"Tricycle"] objectAtIndex:[appDelegate.segueParameter integerValue]];
+    _bicycleField.text = [[_childDetailDictionary objectForKey:@"Bicycle"] objectAtIndex:[appDelegate.segueParameter integerValue]];
+    _trainingWheelsField.text = [[_childDetailDictionary objectForKey:@"TrainingWheels"] objectAtIndex:[appDelegate.segueParameter integerValue]];
+    _schoolField.text = [[_childDetailDictionary objectForKey:@"School"] objectAtIndex:[appDelegate.segueParameter integerValue]];
+    _sleepOverField.text = [[_childDetailDictionary objectForKey:@"Sleepover"] objectAtIndex:[appDelegate.segueParameter integerValue]];
 }
 
 - (BOOL)textFieldShouldBeginEditing:(UITextField *)textField
